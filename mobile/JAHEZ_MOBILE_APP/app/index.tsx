73276c6cceca207b20ed0510f3572c9c43a2482f
Tareq Radi <raditareq15@gmail.com>
2026-04-06 17:51:16 +0300

    feat: initialize JAHEZ mobile app with Expo and Tailwind CSS
    - Add package.json with dependencies and scripts for the mobile app
    - Create tailwind.config.js for Tailwind CSS configuration
    - Add tsconfig.json for TypeScript configuration
    - Remove unused index.ts and text.html files

---
+++ mobile/JAHEZ_MOBILE_APP/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function Index() {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+    </SafeAreaView>
+  );
+}
